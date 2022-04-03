82cb2561f5693bbe837a33d950f2e8604e8e86af
Lukasz W <l.walicki1@gmail.com>
2022-04-03 13:52:06 +0200

    add firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'sundaybootcamp-67f93',
+    appId: '1:777656207743:web:9536bb56ef77efc0de324a',
+    storageBucket: 'sundaybootcamp-67f93.appspot.com',
+    locationId: 'europe-central2',
+    apiKey: 'AIzaSyAnJs2shS5FhTPMf8yVnS4UZNWqpnvek8E',
+    authDomain: 'sundaybootcamp-67f93.firebaseapp.com',
+    messagingSenderId: '777656207743',
+  },
   production: false
 };
 
