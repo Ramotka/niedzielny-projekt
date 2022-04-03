3f99104b06d5dab56bc1a7cf1bc5bc3919d89f88
Magda Ramotowska <rtkmagdalena@gmail.com>
2022-04-03 14:13:57 +0200

    DashboardPage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardPageModule } from './pages/dashboard.page-module';
 
-const routes: Routes = [];
+const routes: Routes = [{ 
+        path: '', 
+        loadChildren: () => DashboardPageModule
+      }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
