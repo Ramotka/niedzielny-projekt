cb727d1bde7e122ab8f736462b8d89b163d95d44
Łukasz Walicki <82345363+lwalik@users.noreply.github.com>
2022-04-04 21:58:05 +0200

    Better version diet section (#6)
    * prettier layout
    
    * create and use onSaveChangesClicked

--- projects/event/src/lib/adapters/primary/ui/diet-list.component-module.ts
+++ projects/event/src/lib/adapters/primary/ui/diet-list.component-module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DietListComponent } from './diet-list.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   declarations: [DietListComponent],
   providers: [],
   exports: [DietListComponent],
