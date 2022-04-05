0bae23c5dd8921baa2e3bde5c6436c666fb1e99b
Lukasz W <l.walicki1@gmail.com>
2022-04-05 23:53:34 +0200

    move all diet components to new Diet Lib

---
+++ projects/diet/src/index.ts
@@ -0,0 +1,4 @@
+export * from './lib/adapters/primary/ui/add-diet-form.component-module';
+export * from './lib/adapters/primary/ui/diet-list.component-module';
+export * from './lib/adapters/secondary/infrastructure/in-memory-input-state.storage-module';
+export * from './lib/adapters/secondary/infrastructure/firebase-diet.service-module';
--- projects/event/src/index.ts
+++ projects/event/src/index.ts
@@ -1,6 +1,2 @@
 export * from './lib/adapters/primary/ui/create-event.component-module';
 export * from './lib/adapters/secondary/infrastructure/firebase-event.service-module';
-export * from './lib/adapters/primary/ui/add-diet-form.component-module';
-export * from './lib/adapters/secondary/infrastructure/firebase-diet.service-module';
-export * from './lib/adapters/primary/ui/diet-list.component-module';
-export * from './lib/adapters/secondary/infrastructure/in-memory-input-state.storage-module';
