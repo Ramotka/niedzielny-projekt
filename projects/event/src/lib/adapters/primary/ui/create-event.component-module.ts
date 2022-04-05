6efca97a219f3a344cce8b678f5c5e82800c91b9
Magda Ramotowska <rtkmagdalena@gmail.com>
2022-04-05 12:19:22 +0200

    Datepicker added

--- projects/event/src/lib/adapters/primary/ui/create-event.component-module.ts
+++ projects/event/src/lib/adapters/primary/ui/create-event.component-module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { CreateEventComponent } from './create-event.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { CreateEventComponent } from "./create-event.component";
+import { ReactiveFormsModule } from "@angular/forms";
+import { BsDatepickerModule } from "ngx-bootstrap/datepicker";
 
-@NgModule({ imports: [CommonModule, ReactiveFormsModule],
-  	declarations: [CreateEventComponent],
-  	providers: [],
-  	exports: [CreateEventComponent] })
-export class CreateEventComponentModule {
-}
+@NgModule({
+  imports: [CommonModule, ReactiveFormsModule, BsDatepickerModule.forRoot()],
+  declarations: [CreateEventComponent],
+  providers: [],
+  exports: [CreateEventComponent],
+})
+export class CreateEventComponentModule {}
