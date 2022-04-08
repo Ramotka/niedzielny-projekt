67f252b17434fda670fff765d2257d91a742926b
Magda Ramotowska <rtkmagdalena@gmail.com>
2022-04-08 11:36:29 +0200

    Context Storage works

--- projects/event/src/lib/adapters/primary/ui/events-list.component.ts
+++ projects/event/src/lib/adapters/primary/ui/events-list.component.ts
@@ -3,17 +3,21 @@ import {
   ViewEncapsulation,
   ChangeDetectionStrategy,
   Inject,
-} from '@angular/core';
-import { Observable } from 'rxjs';
-import { EventDTO } from '../../../application/ports/secondary/event.dto';
+} from "@angular/core";
+import { Observable } from "rxjs";
+import { EventDTO } from "../../../application/ports/secondary/event.dto";
 import {
   GETS_ALL_EVENT_DTO,
   GetsAllEventDtoPort,
-} from '../../../application/ports/secondary/gets-all-event.dto-port';
+} from "../../../application/ports/secondary/gets-all-event.dto-port";
+import {
+  CONTEXT_DTO_STORAGE,
+  ContextDtoStoragePort,
+} from "projects/core/src/lib/application/ports/secondary/context-dto.storage-port";
 
 @Component({
-  selector: 'lib-events-list',
-  templateUrl: './events-list.component.html',
+  selector: "lib-events-list",
+  templateUrl: "./events-list.component.html",
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
@@ -21,6 +25,12 @@ export class EventsListComponent {
   events$: Observable<EventDTO[]> = this._getsAllEventDto.getAll();
 
   constructor(
-    @Inject(GETS_ALL_EVENT_DTO) private _getsAllEventDto: GetsAllEventDtoPort
+    @Inject(GETS_ALL_EVENT_DTO) private _getsAllEventDto: GetsAllEventDtoPort,
+    @Inject(CONTEXT_DTO_STORAGE)
+    private _contextDtoStorage: ContextDtoStoragePort
   ) {}
+
+  onEventClicked(event: EventDTO): void {
+    this._contextDtoStorage.next({ eventId: event.id });
+  }
 }
