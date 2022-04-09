34ec99a4dde97a68dc308665d0f37836ba3a7472
Lukasz W <l.walicki1@gmail.com>
2022-04-09 23:06:19 +0200

    added eventId to diet form

--- projects/diet/src/lib/adapters/primary/ui/add-diet-form.component.ts
+++ projects/diet/src/lib/adapters/primary/ui/add-diet-form.component.ts
@@ -10,6 +10,13 @@ import {
   AddsDietDtoPort,
 } from '../../../application/ports/secondary/adds-diet.dto-port';
 
+import {
+  CONTEXT_DTO_STORAGE,
+  ContextDtoStoragePort,
+} from 'projects/core/src/lib/application/ports/secondary/context-dto.storage-port';
+import { Observable } from 'rxjs';
+import { ContextDTO } from 'projects/core/src/lib/application/ports/secondary/context.dto';
+
 @Component({
   selector: 'lib-add-diet-form',
   templateUrl: './add-diet-form.component.html',
@@ -21,11 +28,19 @@ export class AddDietFormComponent {
     name: new FormControl('', Validators.required),
   });
 
-  constructor(@Inject(ADDS_DIET_DTO) private _addsDietDto: AddsDietDtoPort) {}
+  currentEvent$: Observable<ContextDTO> =
+    this._contextDtoStorage.asObservable();
 
-  onNewDietSubmited(newDiet: FormGroup): void {
+  constructor(
+    @Inject(ADDS_DIET_DTO) private _addsDietDto: AddsDietDtoPort,
+    @Inject(CONTEXT_DTO_STORAGE)
+    private _contextDtoStorage: ContextDtoStoragePort
+  ) {}
+
+  onNewDietSubmited(newDiet: FormGroup, eventId: string): void {
     this._addsDietDto.add({
       name: newDiet.get('name')?.value,
+      eventId: eventId,
     });
     this.newDiet.reset();
   }
