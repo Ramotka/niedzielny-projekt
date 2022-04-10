74a967874785e3c3160715d6094795d00e038f7d
Lukasz W <l.walicki1@gmail.com>
2022-04-11 00:09:07 +0200

    add eventDropdwonListComponent

--- projects/event/src/lib/adapters/primary/ui/event-dropdown-list.component-module.ts
+++ projects/event/src/lib/adapters/primary/ui/event-dropdown-list.component-module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EventDropdownListComponent } from './event-dropdown-list.component';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { RouterModule } from '@angular/router';
 
-@NgModule({ imports: [CommonModule],
-  	declarations: [EventDropdownListComponent],
-  	providers: [],
-  	exports: [EventDropdownListComponent] })
-export class EventDropdownListComponentModule {
-}
+@NgModule({
+  imports: [CommonModule, RouterModule, BsDropdownModule.forRoot()],
+  declarations: [EventDropdownListComponent],
+  providers: [],
+  exports: [EventDropdownListComponent],
+})
+export class EventDropdownListComponentModule {}
--- projects/event/src/lib/adapters/primary/ui/event-dropdown-list.component.ts
+++ projects/event/src/lib/adapters/primary/ui/event-dropdown-list.component.ts
@@ -1,5 +1,42 @@
-import { Component, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+import {
+  Component,
+  ViewEncapsulation,
+  ChangeDetectionStrategy,
+  Inject,
+} from '@angular/core';
+import { Observable } from 'rxjs';
+import { EventDTO } from '../../../application/ports/secondary/event.dto';
+import {
+  GETS_ONE_EVENT_DTO,
+  GetsOneEventDtoPort,
+} from '../../../application/ports/secondary/gets-one-event.dto-port';
+import { switchMap } from 'rxjs/operators';
+import {
+  CONTEXT_DTO_STORAGE,
+  ContextDtoStoragePort,
+} from 'projects/core/src/lib/application/ports/secondary/context-dto.storage-port';
+import {
+  GETS_ALL_EVENT_DTO,
+  GetsAllEventDtoPort,
+} from '../../../application/ports/secondary/gets-all-event.dto-port';
 
-@Component({ selector: 'lib-event-dropdown-list', templateUrl: './event-dropdown-list.component.html', encapsulation: ViewEncapsulation.None, changeDetection: ChangeDetectionStrategy.OnPush })
+@Component({
+  selector: 'lib-event-dropdown-list',
+  templateUrl: './event-dropdown-list.component.html',
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
 export class EventDropdownListComponent {
+  event$: Observable<EventDTO> = this._contextDtoStoragePort
+    .asObservable()
+    .pipe(switchMap((data) => this._getsOneEventDto.getOne(data.eventId)));
+
+  allEvents$: Observable<EventDTO[]> = this._getsAllEventDto.getAll();
+
+  constructor(
+    @Inject(GETS_ONE_EVENT_DTO) private _getsOneEventDto: GetsOneEventDtoPort,
+    @Inject(CONTEXT_DTO_STORAGE)
+    private _contextDtoStoragePort: ContextDtoStoragePort,
+    @Inject(GETS_ALL_EVENT_DTO) private _getsAllEventDto: GetsAllEventDtoPort
+  ) {}
 }
