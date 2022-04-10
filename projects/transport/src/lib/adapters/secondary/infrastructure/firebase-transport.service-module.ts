4965a787d16defcef40f3b9e7672aeb885136384
Lukasz W <l.walicki1@gmail.com>
2022-04-10 21:48:19 +0200

    implement addTransportDtoPort

--- projects/transport/src/lib/adapters/secondary/infrastructure/firebase-transport.service-module.ts
+++ projects/transport/src/lib/adapters/secondary/infrastructure/firebase-transport.service-module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { FirebaseTransportService } from './firebase-transport.service';
+import { ADDS_TRANSPORT_DTO } from '../../../application/ports/secondary/adds-transport.dto-port';
 
-@NgModule({ imports: [AngularFirestoreModule],
-  	declarations: [],
-  	providers: [FirebaseTransportService],
-  	exports: [] })
-export class FirebaseTransportServiceModule {
-}
+@NgModule({
+  imports: [AngularFirestoreModule],
+  declarations: [],
+  providers: [
+    FirebaseTransportService,
+    { provide: ADDS_TRANSPORT_DTO, useExisting: FirebaseTransportService },
+  ],
+  exports: [],
+})
+export class FirebaseTransportServiceModule {}
--- projects/transport/src/lib/adapters/secondary/infrastructure/firebase-transport.service.ts
+++ projects/transport/src/lib/adapters/secondary/infrastructure/firebase-transport.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { AddsTransportDtoPort } from '../../../application/ports/secondary/adds-transport.dto-port';
+import { TransportDTO } from '../../../application/ports/secondary/transport.dto';
 
 @Injectable()
-export class FirebaseTransportService {
-  constructor(private _client: AngularFirestore) {
+export class FirebaseTransportService implements AddsTransportDtoPort {
+  constructor(private _client: AngularFirestore) {}
+
+  add(transport: Partial<TransportDTO>): void {
+    this._client.collection('transports').add(transport);
   }
 }
