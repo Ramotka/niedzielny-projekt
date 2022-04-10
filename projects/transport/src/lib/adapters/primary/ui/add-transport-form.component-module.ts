6ba8434ea28c16118a2ea2d98a48b4e98010a4ee
Lukasz W <l.walicki1@gmail.com>
2022-04-10 21:32:59 +0200

    create addTransportFormComponent

---
+++ projects/transport/src/lib/adapters/primary/ui/add-transport-form.component-module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AddTransportFormComponent } from './add-transport-form.component';
+
+@NgModule({ imports: [CommonModule],
+  	declarations: [AddTransportFormComponent],
+  	providers: [],
+  	exports: [AddTransportFormComponent] })
+export class AddTransportFormComponentModule {
+}
---
+++ projects/transport/src/lib/adapters/primary/ui/add-transport-form.component.ts
@@ -0,0 +1,5 @@
+import { Component, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({ selector: 'lib-add-transport-form', templateUrl: './add-transport-form.component.html', encapsulation: ViewEncapsulation.None, changeDetection: ChangeDetectionStrategy.OnPush })
+export class AddTransportFormComponent {
+}
