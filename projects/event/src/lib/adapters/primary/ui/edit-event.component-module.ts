811af03707fdc7273da7167e797bee45a497a287
Magda Ramotowska <rtkmagdalena@gmail.com>
2022-04-11 11:20:39 +0200

    form template added to edit-event

--- projects/event/src/lib/adapters/primary/ui/edit-event.component-module.ts
+++ projects/event/src/lib/adapters/primary/ui/edit-event.component-module.ts
@@ -1,10 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { EditEventComponent } from './edit-event.component';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { EditEventComponent } from "./edit-event.component";
+import { BsDatepickerModule } from "ngx-bootstrap/datepicker";
 
-@NgModule({ imports: [CommonModule],
-  	declarations: [EditEventComponent],
-  	providers: [],
-  	exports: [EditEventComponent] })
-export class EditEventComponentModule {
-}
+@NgModule({
+  imports: [CommonModule, BsDatepickerModule.forRoot()],
+  declarations: [EditEventComponent],
+  providers: [],
+  exports: [EditEventComponent],
+})
+export class EditEventComponentModule {}
--- projects/event/src/lib/adapters/primary/ui/edit-event.component.ts
+++ projects/event/src/lib/adapters/primary/ui/edit-event.component.ts
@@ -1,5 +1,15 @@
-import { Component, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+import {
+  Component,
+  ViewEncapsulation,
+  ChangeDetectionStrategy,
+} from "@angular/core";
 
-@Component({ selector: 'lib-edit-event', templateUrl: './edit-event.component.html', encapsulation: ViewEncapsulation.None, changeDetection: ChangeDetectionStrategy.OnPush })
+@Component({
+  selector: "lib-edit-event",
+  templateUrl: "./edit-event.component.html",
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
 export class EditEventComponent {
+  readonly minDate: Date = new Date();
 }
