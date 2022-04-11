f49c4c5bda0655063c2c286b2727ddc651181fce
Magda Ramotowska <rtkmagdalena@gmail.com>
2022-04-11 11:55:05 +0200

    editable form in progress

--- projects/event/src/lib/adapters/primary/ui/edit-event.component.ts
+++ projects/event/src/lib/adapters/primary/ui/edit-event.component.ts
@@ -2,7 +2,20 @@ import {
   Component,
   ViewEncapsulation,
   ChangeDetectionStrategy,
+  Inject,
 } from "@angular/core";
+import { Observable } from "rxjs";
+import { switchMap } from "rxjs/operators";
+import { EventDTO } from "../../../application/ports/secondary/event.dto";
+import {
+  GETS_ONE_EVENT_DTO,
+  GetsOneEventDtoPort,
+} from "../../../application/ports/secondary/gets-one-event.dto-port";
+import {
+  CONTEXT_DTO_STORAGE,
+  ContextDtoStoragePort,
+} from "projects/core/src/lib/application/ports/secondary/context-dto.storage-port";
+import { ContextDTO } from "projects/core/src/lib/application/ports/secondary/context.dto";
 
 @Component({
   selector: "lib-edit-event",
@@ -12,4 +25,23 @@ import {
 })
 export class EditEventComponent {
   readonly minDate: Date = new Date();
+  currentEvent$: Observable<
+    EventDTO
+  > = this._contextDtoStoragePort
+    .asObservable()
+    .pipe(
+      switchMap((data: ContextDTO) =>
+        this._getsOneEventDto.getOne(data.eventId)
+      )
+    );
+
+  constructor(
+    @Inject(GETS_ONE_EVENT_DTO) private _getsOneEventDto: GetsOneEventDtoPort,
+    @Inject(CONTEXT_DTO_STORAGE)
+    private _contextDtoStoragePort: ContextDtoStoragePort
+  ) {}
+
+  formatDate(obj: any): Date {
+    return obj.toDate();
+  }
 }
