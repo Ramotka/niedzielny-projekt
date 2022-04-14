d0662901abfa28b421ba91c75301cffb25c68900
Magda Ramotowska <rtkmagdalena@gmail.com>
2022-04-14 17:52:41 +0200

    all attractions DTO ports implemented

--- projects/attractions/src/lib/adapters/secondary/infrastructure/firebase-attractions.service-module.ts
+++ projects/attractions/src/lib/adapters/secondary/infrastructure/firebase-attractions.service-module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { FirebaseAttractionsService } from './firebase-attractions.service';
+import { GETS_ALL_ATTRACTION_DTO } from '../../../application/ports/secondary/gets-all-attraction.dto-port';
+import { ADDS_ATTRACTION_DTO } from '../../../application/ports/secondary/adds-attraction.dto-port';
+import { SETS_ATTRACTION_DTO } from '../../../application/ports/secondary/sets-attraction.dto-port';
+import { REMOVES_ATTRACTION_DTO } from '../../../application/ports/secondary/removes-attraction.dto-port';
 
 @NgModule({ imports: [AngularFirestoreModule],
   	declarations: [],
-  	providers: [FirebaseAttractionsService],
+  	providers: [FirebaseAttractionsService, { provide: GETS_ALL_ATTRACTION_DTO, useExisting: FirebaseAttractionsService }, { provide: ADDS_ATTRACTION_DTO, useExisting: FirebaseAttractionsService }, { provide: SETS_ATTRACTION_DTO, useExisting: FirebaseAttractionsService }, { provide: REMOVES_ATTRACTION_DTO, useExisting: FirebaseAttractionsService }],
   	exports: [] })
 export class FirebaseAttractionsServiceModule {
 }
