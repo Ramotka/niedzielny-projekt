dfbfc3b2b23ac6152027833f86fc1cd8bd2d343e
Magda Ramotowska <rtkmagdalena@gmail.com>
2022-04-14 21:53:04 +0200

    add-attractions component finished

--- projects/attractions/src/lib/adapters/primary/ui/add-attraction-form.component.ts
+++ projects/attractions/src/lib/adapters/primary/ui/add-attraction-form.component.ts
@@ -1,9 +1,20 @@
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import {
   Component,
   ViewEncapsulation,
   ChangeDetectionStrategy,
+  Inject,
 } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import {
+  ADDS_ATTRACTION_DTO,
+  AddsAttractionDtoPort,
+} from '../../../application/ports/secondary/adds-attraction.dto-port';
+import {
+  CONTEXT_DTO_STORAGE,
+  ContextDtoStoragePort,
+} from 'projects/core/src/lib/application/ports/secondary/context-dto.storage-port';
+import { ContextDTO } from 'projects/core/src/lib/application/ports/secondary/context.dto';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'lib-add-attraction-form',
@@ -15,4 +26,22 @@ export class AddAttractionFormComponent {
   readonly newAttraction: FormGroup = new FormGroup({
     name: new FormControl('', Validators.required),
   });
+
+  currentEvent$: Observable<ContextDTO> =
+    this._contextDtoStorage.asObservable();
+
+  constructor(
+    @Inject(ADDS_ATTRACTION_DTO)
+    private _addsAttractionDto: AddsAttractionDtoPort,
+    @Inject(CONTEXT_DTO_STORAGE)
+    private _contextDtoStorage: ContextDtoStoragePort
+  ) {}
+
+  onNewAttractionSubmited(newAttraction: FormGroup, eventId: string): void {
+    this._addsAttractionDto.add({
+      name: newAttraction.get('name')?.value,
+      eventId: eventId,
+    });
+    this.newAttraction.reset();
+  }
 }
--- src/app/pages/attractions.page-module.ts
+++ src/app/pages/attractions.page-module.ts
@@ -3,18 +3,22 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { AttractionsPage } from './attractions.page';
 import { AddAttractionFormComponentModule } from '../../../projects/attractions/src/lib/adapters/primary/ui/add-attraction-form.component-module';
+import { FirebaseAttractionsServiceModule } from '@attractions';
 
-@NgModule({ imports: [CommonModule, 
-      RouterModule.forChild([
-        {
-          path: '',
-          component: AttractionsPage,
-        }
-      ]),
-  AddAttractionFormComponentModule
-],
-  	declarations: [AttractionsPage],
-  	providers: [],
-  	exports: [] })
-export class AttractionsPageModule {
-}
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: AttractionsPage,
+      },
+    ]),
+    AddAttractionFormComponentModule,
+    FirebaseAttractionsServiceModule,
+  ],
+  declarations: [AttractionsPage],
+  providers: [],
+  exports: [],
+})
+export class AttractionsPageModule {}
