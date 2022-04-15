041071fece818f3d499057fba898108322710e10
Lukasz W <l.walicki1@gmail.com>
2022-04-15 22:07:19 +0200

    create inMemorySearchStorage

--- projects/event/src/index.ts
+++ projects/event/src/index.ts
@@ -5,3 +5,4 @@ export * from './lib/adapters/primary/ui/event-dashboard.component-module';
 export * from './lib/adapters/primary/ui/event-id.resolver-module';
 export * from './lib/adapters/primary/ui/event-dropdown-list.component-module';
 export * from './lib/adapters/primary/ui/search-event.component-module';
+export * from './lib/adapters/secondary/infrastructure/in-memory-search.storage-module';
