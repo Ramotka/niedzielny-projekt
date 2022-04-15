8acd136157627fa70b97ccb42d62bb014aeda777
Lukasz W <l.walicki1@gmail.com>
2022-04-15 23:55:44 +0200

    create and implement confirmDeleteEventModalComponent

--- projects/event/src/lib/adapters/primary/ui/events-list.component-module.ts
+++ projects/event/src/lib/adapters/primary/ui/events-list.component-module.ts
@@ -3,9 +3,15 @@ import { CommonModule } from '@angular/common';
 import { EventsListComponent } from './events-list.component';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 @NgModule({
-  imports: [CommonModule, RouterModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    ReactiveFormsModule,
+    ModalModule.forRoot(),
+  ],
   declarations: [EventsListComponent],
   providers: [],
   exports: [EventsListComponent],
