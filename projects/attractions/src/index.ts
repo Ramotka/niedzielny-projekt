d7a4cfd83779532dba59f38379ccc1674ff64d08
Lukasz W <l.walicki1@gmail.com>
2022-04-15 22:57:23 +0200

    create attractionsCardComponent

--- projects/attractions/src/index.ts
+++ projects/attractions/src/index.ts
@@ -2,3 +2,4 @@ export * from './lib/adapters/secondary/infrastructure/firebase-attractions.serv
 export * from './lib/adapters/primary/ui/add-attraction-form.component-module';
 export * from './lib/adapters/primary/ui/attractions-list.component-module';
 export * from './lib/adapters/secondary/infrastructure/in-memory-input-state.storage-module';
+export * from './lib/adapters/primary/ui/attractions-card.component-module';
