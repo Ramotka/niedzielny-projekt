a3f682df84db77da4a722e70732c9af64bec0858
Lukasz W <l.walicki1@gmail.com>
2022-04-17 09:25:50 +0200

    replace date with fromDate and toDate in eventDto

--- projects/event/src/lib/application/ports/secondary/event.dto.ts
+++ projects/event/src/lib/application/ports/secondary/event.dto.ts
@@ -2,6 +2,7 @@ export interface EventDTO {
   readonly imageUrl: string;
   readonly description: string;
   readonly title: string;
-  readonly date: Array<Date>;
+  readonly fromDate: Date;
+  readonly toDate: Date;
   readonly id: string;
 }
