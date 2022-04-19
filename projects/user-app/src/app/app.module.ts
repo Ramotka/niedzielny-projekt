3122f0d9b9f1200b0a89f9a1129111a01a006219
Lukasz W <l.walicki1@gmail.com>
2022-04-19 20:21:53 +0200

    create user-app

---
+++ projects/user-app/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { BrowserModule } from '@angular/platform-browser';
+import { environment } from '../environments/environment';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(environment.firebase),
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
