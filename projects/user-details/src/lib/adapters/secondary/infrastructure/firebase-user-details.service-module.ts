58baba34bbd1d1c39aeb928b4558379b5a6702df
Lukasz W <l.walicki1@gmail.com>
2022-04-23 00:36:39 +0200

    implement removesUserDetailsDtoPort

--- projects/user-details/src/lib/adapters/secondary/infrastructure/firebase-user-details.service-module.ts
+++ projects/user-details/src/lib/adapters/secondary/infrastructure/firebase-user-details.service-module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { FirebaseUserDetailsService } from './firebase-user-details.service';
+import { REMOVES_USER_DETAILS_DTO } from '../../../application/ports/secondary/removes-user-details.dto-port';
 
 @NgModule({ imports: [AngularFirestoreModule],
   	declarations: [],
-  	providers: [FirebaseUserDetailsService],
+  	providers: [FirebaseUserDetailsService, { provide: REMOVES_USER_DETAILS_DTO, useExisting: FirebaseUserDetailsService }],
   	exports: [] })
 export class FirebaseUserDetailsServiceModule {
 }
--- projects/user-details/src/lib/adapters/secondary/infrastructure/firebase-user-details.service.ts
+++ projects/user-details/src/lib/adapters/secondary/infrastructure/firebase-user-details.service.ts
@@ -1,7 +1,13 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
+import { RemovesUserDetailsDtoPort } from '../../../application/ports/secondary/removes-user-details.dto-port';
 
 @Injectable()
-export class FirebaseUserDetailsService {
-  constructor(private _auth: AngularFireAuth) {}
+export class FirebaseUserDetailsService implements RemovesUserDetailsDtoPort {
+  constructor(private _auth: AngularFireAuth, private _router: Router) {}
+
+  remove(id: string): void {
+    this._auth.signOut().then(() => this._router.navigate(['']));
+  }
 }
