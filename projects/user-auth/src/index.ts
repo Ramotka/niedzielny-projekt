8fd1ecbcd4f244b5bfffdd3ca2680e88e6c1a10a
Lukasz W <l.walicki1@gmail.com>
2022-04-23 00:03:24 +0200

    create userDetails lib

--- projects/user-auth/src/index.ts
+++ projects/user-auth/src/index.ts
@@ -1,5 +1,4 @@
 export * from './lib/adapters/secondary/infrastructure/firebase-auth.service-module';
 export * from './lib/adapters/primary/ui/user.component-module';
-export * from './lib/adapters/primary/ui/profile-user.component-module';
 export * from './lib/adapters/primary/ui/create-user.component-module';
 export * from './lib/adapters/primary/ui/completed-create-user-modal.component-module';
