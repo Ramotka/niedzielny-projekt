d12f0546efc7a3ce92fc1025a23eaf489b5b3ffd
Lukasz W <l.walicki1@gmail.com>
2022-04-23 00:32:21 +0200

    create newUserDetails form

--- projects/user-details/src/lib/adapters/primary/ui/create-user-details.component.ts
+++ projects/user-details/src/lib/adapters/primary/ui/create-user-details.component.ts
@@ -1,5 +1,20 @@
-import { Component, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+import {
+  Component,
+  ViewEncapsulation,
+  ChangeDetectionStrategy,
+} from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
-@Component({ selector: 'lib-create-user-details', templateUrl: './create-user-details.component.html', encapsulation: ViewEncapsulation.None, changeDetection: ChangeDetectionStrategy.OnPush })
+@Component({
+  selector: 'lib-create-user-details',
+  templateUrl: './create-user-details.component.html',
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
 export class CreateUserDetailsComponent {
+  readonly newUserDetails: FormGroup = new FormGroup({
+    name: new FormControl(),
+    lastName: new FormControl(),
+    email: new FormControl(),
+  });
 }
