54d6ec0a726ff575196dda863826554fe00457dc
Lukasz W <l.walicki1@gmail.com>
2022-04-22 23:47:06 +0200

    add routes

--- projects/user-app/src/app/app-routing.module.ts
+++ projects/user-app/src/app/app-routing.module.ts
@@ -1,11 +1,46 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageModule } from './pages/home.page-module';
+import {
+  AngularFireAuthGuard,
+  redirectLoggedInTo,
+  redirectUnauthorizedTo,
+} from '@angular/fire/compat/auth-guard';
+import { ProfileUserPageModule } from './pages/profile-user.page-module';
+import { NotFoundPageModule } from './pages/not-found.page-module';
+import { LoginPageModule } from './pages/login.page-module';
+import { RegisterPageModule } from './pages/register.page-module';
+
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['login']);
+const redirectLoggedInToItems = () => redirectLoggedInTo(['items']);
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () => HomePageModule,
+  },
+  {
+    path: 'profile',
+    loadChildren: () => ProfileUserPageModule,
+    canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: redirectUnauthorizedToLogin },
+  },
+
+  {
+    path: 'login',
+    loadChildren: () => LoginPageModule,
+  },
+  {
+    path: 'register',
+    loadChildren: () => RegisterPageModule,
+  },
+  {
+    path: '404',
+    loadChildren: () => NotFoundPageModule,
+  },
+  {
+    path: '**',
+    redirectTo: '404',
   },
 ];
 
