197d68948ab59206c88d28b522e0f57e1bde3622
Lukasz W <l.walicki1@gmail.com>
2022-04-24 20:40:50 +0200

    change userDetailsPage layout

--- projects/core/src/lib/application/ports/secondary/context-dto.storage-port.ts
+++ projects/core/src/lib/application/ports/secondary/context-dto.storage-port.ts
@@ -2,7 +2,9 @@ import { InjectionToken } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ContextDTO } from './context.dto';
 
-export const CONTEXT_DTO_STORAGE = new InjectionToken<ContextDtoStoragePort>('CONTEXT_DTO_STORAGE');
+export const CONTEXT_DTO_STORAGE = new InjectionToken<ContextDtoStoragePort>(
+  'CONTEXT_DTO_STORAGE'
+);
 
 export interface ContextDtoStoragePort {
   next(item: Partial<ContextDTO | undefined>): void;
