004f593a9fd4b84980fe424fc8818b6ab3949eef
Lukasz W <l.walicki1@gmail.com>
2022-04-24 23:14:17 +0200

    change tree

--- apps/user-app/src/app/pages/home.page-module.ts
+++ apps/user-app/src/app/pages/home.page-module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { HomePage } from './home.page';
-import { UserComponentModule } from '../../../../user-auth/src/lib/adapters/primary/ui/user.component-module';
-import { FirebaseAuthServiceModule } from '../../../../user-auth/src/lib/adapters/secondary/infrastructure/firebase-auth.service-module';
-import { CreateUserComponentModule } from '../../../../user-auth/src/lib/adapters/primary/ui/create-user.component-module';
+import { UserComponentModule } from 'projects/user-auth/src/lib/adapters/primary/ui/user.component-module';
+import { FirebaseAuthServiceModule } from 'projects/user-auth/src/lib/adapters/secondary/infrastructure/firebase-auth.service-module';
+import { CreateUserComponentModule } from 'projects/user-auth/src/lib/adapters/primary/ui/create-user.component-module';
 
 @NgModule({
   imports: [
