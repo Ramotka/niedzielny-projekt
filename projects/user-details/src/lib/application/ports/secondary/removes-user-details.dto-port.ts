16e8de9db86cdff246f99e23db5ae6d8dd2a5318
Lukasz W <l.walicki1@gmail.com>
2022-04-24 20:44:07 +0200

    add userId in userDetailsDto

--- projects/user-details/src/lib/application/ports/secondary/removes-user-details.dto-port.ts
+++
@@ -1,8 +0,0 @@
-import { InjectionToken } from '@angular/core';
-
-export const REMOVES_USER_DETAILS_DTO =
-  new InjectionToken<RemovesUserDetailsDtoPort>('REMOVES_USER_DETAILS_DTO');
-
-export interface RemovesUserDetailsDtoPort {
-  remove(id: string): void;
-}
--- projects/user-details/src/lib/application/ports/secondary/user-details.dto.ts
+++ projects/user-details/src/lib/application/ports/secondary/user-details.dto.ts
@@ -2,4 +2,5 @@ export interface UserDetailsDTO {
   readonly email: string;
   readonly name: string;
   readonly lastName: string;
+  readonly userId: string;
 }
