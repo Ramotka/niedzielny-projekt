73e0bc0c8df4a46765c2984a84182abb07f7e8cc
Lukasz W <l.walicki1@gmail.com>
2022-04-25 20:59:47 +0200

    add userEventsListComponent to userEventsPage

--- apps/user-app/src/app/pages/user-events.page-module.ts
+++ apps/user-app/src/app/pages/user-events.page-module.ts
@@ -2,16 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { UserEventsPage } from './user-events.page';
+import { UserEventsListComponentModule } from 'libs/event/src/lib/adapters/primary/ui/user-events-list.component-module';
+import { FirebaseUserDetailsServiceModule } from 'libs/user-details/src/lib/adapters/secondary/infrastructure/firebase-user-details.service-module';
+import { FirebaseParticipantServiceModule } from '@participant';
 
-@NgModule({ imports: [CommonModule, 
-      RouterModule.forChild([
-        {
-          path: '',
-          component: UserEventsPage,
-        }
-      ])],
-  	declarations: [UserEventsPage],
-  	providers: [],
-  	exports: [] })
-export class UserEventsPageModule {
-}
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: UserEventsPage,
+      },
+    ]),
+    UserEventsListComponentModule,
+    FirebaseUserDetailsServiceModule,
+    FirebaseParticipantServiceModule,
+  ],
+  declarations: [UserEventsPage],
+  providers: [],
+  exports: [],
+})
+export class UserEventsPageModule {}
