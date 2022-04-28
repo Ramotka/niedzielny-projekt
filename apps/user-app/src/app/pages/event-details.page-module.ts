6eb345c894861a052aed78765512f00acfaf7238
Lukasz W <l.walicki1@gmail.com>
2022-04-29 00:59:03 +0200

    create eventDetailsPage

---
+++ apps/user-app/src/app/pages/event-details.page-module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { EventDetailsPage } from './event-details.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: EventDetailsPage,
+      },
+    ]),
+  ],
+  declarations: [EventDetailsPage],
+  providers: [],
+  exports: [],
+})
+export class EventDetailsPageModule {}
