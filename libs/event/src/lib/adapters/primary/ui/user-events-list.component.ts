dc0a82bb2987c4a2d7c98c73e821ef8d31db97e6
Lukasz W <l.walicki1@gmail.com>
2022-04-29 22:02:16 +0200

    change userEventsList

--- libs/event/src/lib/adapters/primary/ui/user-events-list.component.ts
+++ libs/event/src/lib/adapters/primary/ui/user-events-list.component.ts
@@ -4,19 +4,30 @@ import {
   ChangeDetectionStrategy,
   Inject,
 } from '@angular/core';
-import { Observable } from 'rxjs';
+import { combineLatest, Observable } from 'rxjs';
 
 import { ParticipantDTO } from '../../../../../../participant/src/lib/application/ports/secondary/participant.dto';
 import {
   GETS_ALL_PARTICIPANT_DTO,
   GetsAllParticipantDtoPort,
 } from '../../../../../../participant/src/lib/application/ports/secondary/gets-all-participant.dto-port';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, map } from 'rxjs/operators';
 import {
   CURRENT_USER_DTO_STORAGE,
   CurrentUserDtoStoragePort,
 } from '../../../../../../core/src/lib/application/ports/secondary/current-user-dto.storage-port';
 import { CurrentUserDTO } from '../../../../../../core/src/lib/application/ports/secondary/current-user.dto';
+import { EventDTO } from '../../../application/ports/secondary/event.dto';
+import {
+  GETS_ALL_EVENT_DTO,
+  GetsAllEventDtoPort,
+} from '../../../application/ports/secondary/gets-all-event.dto-port';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
+import {
+  ContextDtoStoragePort,
+  CONTEXT_DTO_STORAGE,
+} from 'libs/core/src/lib/application/ports/secondary/context-dto.storage-port';
 
 @Component({
   selector: 'lib-user-events-list',
@@ -25,16 +36,52 @@ import { CurrentUserDTO } from '../../../../../../core/src/lib/application/ports
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UserEventsListComponent {
-  participants$: Observable<ParticipantDTO[]> =
-    this._getsAllParticipantDto.getAll();
+  participants$: Observable<ParticipantDTO[]> = this._currentUserDtoStorage
+    .asObservable()
+    .pipe(
+      switchMap((data) =>
+        this._getsAllParticipantDto.getAll({ email: data.userEmail })
+      )
+    );
 
-  // currentUser$: Observable<CurrentUserDTO> =
-  //   this._currentUserDtoStorage.asObservable();
+  // userEvents$: Observable<EventDTO[]> = 
+  // userEvents$: Observable<EventDTO[]> = this._currentUserDtoStorage
+  //   .asObservable()
+  //   .pipe(
+  //     switchMap((data) =>
+  //       this._getsAllParticipantDto
+  //         .getAll({ email: data.userEmail })
+  //         .pipe(
+  //           switchMap((data) =>
+  //             this._getsAllEventDto.getAll({
+  //               id: data.forEach((item) => item.eventId),
+  //             })
+  //           )
+  //         )
+  //     )
+  //   );
+
+  onTakeButtonClicked(participant: Partial<ParticipantDTO>): void {
+    // this._getsAllEventDto.getAll({ id: participant.eventId });
+    this._contextDtoStoragePort.next({ eventId: participant.eventId });
+
+    this._router.navigate(['my-events/' + participant.eventId]);
+  }
+
+  onLogOutButtonClicked(): void {
+    // this._userSignOutDto.userSingOut();
+    this._auth.signOut().then(() => this._router.navigate(['']));
+  }
 
   constructor(
     @Inject(GETS_ALL_PARTICIPANT_DTO)
     private _getsAllParticipantDto: GetsAllParticipantDtoPort,
     @Inject(CURRENT_USER_DTO_STORAGE)
-    private _currentUserDtoStorage: CurrentUserDtoStoragePort
+    private _currentUserDtoStorage: CurrentUserDtoStoragePort,
+    @Inject(GETS_ALL_EVENT_DTO) private _getsAllEventDto: GetsAllEventDtoPort,
+    @Inject(CONTEXT_DTO_STORAGE)
+    private _contextDtoStoragePort: ContextDtoStoragePort,
+    private _auth: AngularFireAuth,
+    private _router: Router
   ) {}
 }
