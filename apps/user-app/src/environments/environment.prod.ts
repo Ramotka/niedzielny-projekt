e4e46029a836aa5b34907841086e26764285e94e
Lukasz W <l.walicki1@gmail.com>
2022-05-01 18:03:45 +0200

    deploy user-app on firebase hosting

--- apps/user-app/src/environments/environment.prod.ts
+++ apps/user-app/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  firebase: {
+    projectId: 'sundaybootcamp-67f93',
+    appId: '1:777656207743:web:9536bb56ef77efc0de324a',
+    storageBucket: 'sundaybootcamp-67f93.appspot.com',
+    locationId: 'europe-central2',
+    apiKey: 'AIzaSyAnJs2shS5FhTPMf8yVnS4UZNWqpnvek8E',
+    authDomain: 'sundaybootcamp-67f93.firebaseapp.com',
+    messagingSenderId: '777656207743',
+  },
+  production: true,
 };
