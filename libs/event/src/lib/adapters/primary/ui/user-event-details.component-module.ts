72306e2016b87999465ee790c5b19cdcb5683df5
Lukasz W <l.walicki1@gmail.com>
2022-05-01 12:55:40 +0200

    add events list in userEventDetailsComponent

--- libs/event/src/lib/adapters/primary/ui/user-event-details.component-module.ts
+++ libs/event/src/lib/adapters/primary/ui/user-event-details.component-module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserEventDetailsComponent } from './user-event-details.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   declarations: [UserEventDetailsComponent],
   providers: [],
   exports: [UserEventDetailsComponent],
--- libs/event/src/lib/adapters/primary/ui/user-event-details.component.ts
+++ libs/event/src/lib/adapters/primary/ui/user-event-details.component.ts
@@ -2,7 +2,20 @@ import {
   Component,
   ViewEncapsulation,
   ChangeDetectionStrategy,
+  Inject,
 } from '@angular/core';
+import { Observable } from 'rxjs';
+import { EventDTO } from '../../../application/ports/secondary/event.dto';
+import {
+  GETS_ONE_EVENT_DTO,
+  GetsOneEventDtoPort,
+} from '../../../application/ports/secondary/gets-one-event.dto-port';
+import { switchMap } from 'rxjs/operators';
+import {
+  CONTEXT_DTO_STORAGE,
+  ContextDtoStoragePort,
+} from 'libs/core/src/lib/application/ports/secondary/context-dto.storage-port';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'lib-user-event-details',
@@ -10,4 +23,19 @@ import {
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class UserEventDetailsComponent {}
+export class UserEventDetailsComponent {
+  event$: Observable<EventDTO> = this._contextDtoStoragePort
+    .asObservable()
+    .pipe(switchMap((data) => this._getsOneEventDto.getOne(data.eventId)));
+
+  constructor(
+    @Inject(GETS_ONE_EVENT_DTO) private _getsOneEventDto: GetsOneEventDtoPort,
+    @Inject(CONTEXT_DTO_STORAGE)
+    private _contextDtoStoragePort: ContextDtoStoragePort,
+    private _router: Router
+  ) {}
+
+  onConfirmButtonClicked(event: EventDTO): void {
+    this._router.navigate(['setup']);
+  }
+}
