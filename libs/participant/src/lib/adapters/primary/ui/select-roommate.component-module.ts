fe24488e4bc414d23984b4704bc66c6951db4ff9
Lukasz W <l.walicki1@gmail.com>
2022-05-02 18:31:29 +0200

    add routerLink in selectRoommateComponent

--- libs/participant/src/lib/adapters/primary/ui/select-roommate.component-module.ts
+++ libs/participant/src/lib/adapters/primary/ui/select-roommate.component-module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SelectRoommateComponent } from './select-roommate.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
   declarations: [SelectRoommateComponent],
   providers: [],
   exports: [SelectRoommateComponent],
