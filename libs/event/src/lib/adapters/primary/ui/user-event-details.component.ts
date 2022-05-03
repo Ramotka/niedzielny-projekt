4e22ae02a680f28f9111ef31e91311ca4e8fcc3b
Lukasz W <l.walicki1@gmail.com>
2022-05-03 02:16:18 +0200

    create and use onConfirmButtonClicked and onCancelButtonClicked functions

--- libs/event/src/lib/adapters/primary/ui/user-event-details.component.ts
+++ libs/event/src/lib/adapters/primary/ui/user-event-details.component.ts
@@ -4,7 +4,7 @@ import {
   ChangeDetectionStrategy,
   Inject,
 } from '@angular/core';
-import { Observable } from 'rxjs';
+import { combineLatest, Observable } from 'rxjs';
 import { EventDTO } from '../../../application/ports/secondary/event.dto';
 import {
   GETS_ONE_EVENT_DTO,
@@ -16,6 +16,20 @@ import {
   ContextDtoStoragePort,
 } from 'libs/core/src/lib/application/ports/secondary/context-dto.storage-port';
 import { Router } from '@angular/router';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import {
+  SETS_PARTICIPANT_DTO,
+  SetsParticipantDtoPort,
+} from 'libs/participant/src/lib/application/ports/secondary/sets-participant.dto-port';
+import { ParticipantDTO } from 'libs/participant/src/lib/application/ports/secondary/participant.dto';
+import {
+  GETS_ONE_PARTICIPANT_DTO,
+  GetsOneParticipantDtoPort,
+} from 'libs/participant/src/lib/application/ports/secondary/gets-one-participant.dto-port';
+import {
+  CurrentUserDtoStoragePort,
+  CURRENT_USER_DTO_STORAGE,
+} from 'libs/core/src/lib/application/ports/secondary/current-user-dto.storage-port';
 
 @Component({
   selector: 'lib-user-event-details',
@@ -28,14 +42,48 @@ export class UserEventDetailsComponent {
     .asObservable()
     .pipe(switchMap((data) => this._getsOneEventDto.getOne(data.eventId)));
 
+  participant$: Observable<ParticipantDTO> = combineLatest([
+    this._contextDtoStoragePort.asObservable(),
+    this._currentUserDtoStoragePort.asObservable(),
+  ]).pipe(
+    switchMap(([context, currentUser]) =>
+      this._getsOneParticipantDto.getOne({
+        eventId: context.eventId,
+        email: currentUser.userEmail,
+      })
+    )
+  );
+
   constructor(
     @Inject(GETS_ONE_EVENT_DTO) private _getsOneEventDto: GetsOneEventDtoPort,
     @Inject(CONTEXT_DTO_STORAGE)
     private _contextDtoStoragePort: ContextDtoStoragePort,
-    private _router: Router
+    private _router: Router,
+    private modalService: BsModalService,
+    @Inject(SETS_PARTICIPANT_DTO)
+    private _setsParticipantDto: SetsParticipantDtoPort,
+    @Inject(GETS_ONE_PARTICIPANT_DTO)
+    private _getsOneParticipantDto: GetsOneParticipantDtoPort,
+    @Inject(CURRENT_USER_DTO_STORAGE)
+    private _currentUserDtoStoragePort: CurrentUserDtoStoragePort
   ) {}
 
-  onConfirmButtonClicked(event: EventDTO): void {
-    this._router.navigate(['setup']);
+  modalRef?: BsModalRef;
+
+  onConfirmButtonClicked(participantId: string): void {
+    const baseUrl = this._router.url;
+    this._setsParticipantDto.set({
+      id: participantId,
+      status: true,
+    });
+    this._router.navigate([baseUrl + '/setup']);
+  }
+
+  onCancelButtonClicked(participantId: string): void {
+    this._setsParticipantDto.set({
+      id: participantId,
+      status: false,
+    });
+    this._router.navigate(['my-events']);
   }
 }
