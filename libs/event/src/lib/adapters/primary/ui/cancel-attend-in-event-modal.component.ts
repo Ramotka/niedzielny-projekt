2fd8d5ff753266de0f2ef6f1864a87300446c818
Lukasz W <l.walicki1@gmail.com>
2022-05-03 02:20:22 +0200

    create canceclAttendInModalComponent

---
+++ libs/event/src/lib/adapters/primary/ui/cancel-attend-in-event-modal.component.ts
@@ -0,0 +1,13 @@
+import {
+  Component,
+  ViewEncapsulation,
+  ChangeDetectionStrategy,
+} from '@angular/core';
+
+@Component({
+  selector: 'lib-cancel-attend-in-event-modal',
+  templateUrl: './cancel-attend-in-event-modal.component.html',
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CancelAttendInEventModalComponent {}
