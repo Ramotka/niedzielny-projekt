a96144c87761f95b71faac38a36fe977c37ba2be
Lukasz W <l.walicki1@gmail.com>
2022-05-15 23:23:47 +0200

    create and implement inputStateDtoStoragePort

--- libs/room/src/lib/adapters/secondary/infrastructure/in-memory-input-state.storage.ts
+++ libs/room/src/lib/adapters/secondary/infrastructure/in-memory-input-state.storage.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@angular/core';
+import { ReplaySubject } from 'rxjs';
+import { InputStateDTO } from '../../../application/ports/secondary/input-state.dto';
+import { InputStateDtoStoragePort } from '../../../application/ports/secondary/input-state-dto.storage-port';
 
 @Injectable()
-export class InMemoryInputStateStorage {}
+export class InMemoryInputStateStorage
+  extends ReplaySubject<InputStateDTO>
+  implements InputStateDtoStoragePort {}
