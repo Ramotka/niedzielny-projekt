9ff765c8024f48888c7f892ae04f995dbe66107a
Lukasz W <l.walicki1@gmail.com>
2022-05-15 22:28:50 +0200

    implement addsRoomDtoPort

--- libs/room/src/lib/adapters/secondary/infrastructure/firebase-room.service-module.ts
+++ libs/room/src/lib/adapters/secondary/infrastructure/firebase-room.service-module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { FirebaseRoomService } from './firebase-room.service';
+import { ADDS_ROOM_DTO } from '../../../application/ports/secondary/adds-room.dto-port';
 
 @NgModule({
   imports: [AngularFirestoreModule],
   declarations: [],
-  providers: [FirebaseRoomService],
+  providers: [
+    FirebaseRoomService,
+    { provide: ADDS_ROOM_DTO, useExisting: FirebaseRoomService },
+  ],
   exports: [],
 })
 export class FirebaseRoomServiceModule {}
--- libs/room/src/lib/adapters/secondary/infrastructure/firebase-room.service.ts
+++ libs/room/src/lib/adapters/secondary/infrastructure/firebase-room.service.ts
@@ -1,7 +1,13 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { AddsRoomDtoPort } from '../../../application/ports/secondary/adds-room.dto-port';
+import { RoomDTO } from '../../../application/ports/secondary/room.dto';
 
 @Injectable()
-export class FirebaseRoomService {
+export class FirebaseRoomService implements AddsRoomDtoPort {
   constructor(private _client: AngularFirestore) {}
+
+  add(room: Partial<RoomDTO>): void {
+    this._client.collection('rooms').add(room);
+  }
 }
