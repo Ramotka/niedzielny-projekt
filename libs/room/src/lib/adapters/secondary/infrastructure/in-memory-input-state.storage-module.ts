2089b9ebe3db821807f954b3a9fb5cfc816deba7
Lukasz W <l.walicki1@gmail.com>
2022-05-15 23:22:46 +0200

    create inMemoryInputStateStorage

---
+++ libs/room/src/lib/adapters/secondary/infrastructure/in-memory-input-state.storage-module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { InMemoryInputStateStorage } from './in-memory-input-state.storage';
+
+@NgModule({
+  imports: [],
+  declarations: [],
+  providers: [InMemoryInputStateStorage],
+  exports: [],
+})
+export class InMemoryInputStateStorageModule {}
