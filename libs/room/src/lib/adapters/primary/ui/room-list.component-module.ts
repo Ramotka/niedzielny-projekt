50089918fb53ef7ba631ddf48153978b6e71f9fd
Lukasz W <l.walicki1@gmail.com>
2022-05-16 00:17:35 +0200

    create roomListComponent layout

--- libs/room/src/lib/adapters/primary/ui/room-list.component-module.ts
+++ libs/room/src/lib/adapters/primary/ui/room-list.component-module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RoomListComponent } from './room-list.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   declarations: [RoomListComponent],
   providers: [],
   exports: [RoomListComponent],
