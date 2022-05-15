e968ca175a21f0c302d5409a5432fef53f744d72
Lukasz W <l.walicki1@gmail.com>
2022-05-15 23:00:27 +0200

    create roomListComponent

--- libs/room/src/index.ts
+++ libs/room/src/index.ts
@@ -1,2 +1,3 @@
 export * from './lib/adapters/secondary/infrastructure/firebase-room.service-module';
 export * from './lib/adapters/primary/ui/create-room-form.component-module';
+export * from './lib/adapters/primary/ui/room-list.component-module';
---
+++ libs/room/src/lib/adapters/primary/ui/room-list.component-module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RoomListComponent } from './room-list.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [RoomListComponent],
+  providers: [],
+  exports: [RoomListComponent],
+})
+export class RoomListComponentModule {}
