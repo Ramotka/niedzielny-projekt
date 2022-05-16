d33fb7b09e269bdb7b95f03d71bec6c02a891766
Magda Ramotowska <rtkmagdalena@gmail.com>
2022-05-16 17:49:00 +0200

    rooms filtered by eventID

--- libs/room/src/lib/adapters/primary/components/select-room.component.ts
+++ libs/room/src/lib/adapters/primary/components/select-room.component.ts
@@ -4,12 +4,16 @@ import {
   ChangeDetectionStrategy,
   Inject,
 } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, switchMap } from 'rxjs';
 import { RoomDTO } from '../../../application/ports/secondary/room.dto';
 import {
   GETS_ALL_ROOM_DTO,
   GetsAllRoomDtoPort,
 } from '../../../application/ports/secondary/gets-all-room.dto-port';
+import {
+  ContextDtoStoragePort,
+  CONTEXT_DTO_STORAGE,
+} from 'libs/core/src/lib/application/ports/secondary/context-dto.storage-port';
 
 @Component({
   selector: 'lib-select-room',
@@ -18,9 +22,17 @@ import {
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SelectRoomComponent {
-  availableRooms$: Observable<RoomDTO[]> = this._getsAllRoomDto.getAll();
+  availableRooms$: Observable<RoomDTO[]> = this._contextDtoStoragePort
+    .asObservable()
+    .pipe(
+      switchMap((data) =>
+        this._getsAllRoomDto.getAll({ eventId: data.eventId })
+      )
+    );
 
   constructor(
-    @Inject(GETS_ALL_ROOM_DTO) private _getsAllRoomDto: GetsAllRoomDtoPort
+    @Inject(GETS_ALL_ROOM_DTO) private _getsAllRoomDto: GetsAllRoomDtoPort,
+    @Inject(CONTEXT_DTO_STORAGE)
+    private _contextDtoStoragePort: ContextDtoStoragePort
   ) {}
 }
