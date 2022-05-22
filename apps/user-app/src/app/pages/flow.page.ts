f8c1198306814015b1d7f1bd02f395fb9cbd390a
Lukasz W <l.walicki1@gmail.com>
2022-05-22 13:43:55 +0200

    create flowPage

---
+++ apps/user-app/src/app/pages/flow.page.ts
@@ -0,0 +1,5 @@
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+
+@Component({ selector: 'app-flow-page', templateUrl: './flow.page.html', encapsulation: ViewEncapsulation.None, changeDetection: ChangeDetectionStrategy.OnPush })
+export class FlowPage {
+}
