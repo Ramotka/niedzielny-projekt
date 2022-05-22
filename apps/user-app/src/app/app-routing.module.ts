26350332bca0487bd4bb6607028e29bbeedad789
Lukasz W <l.walicki1@gmail.com>
2022-05-22 13:58:49 +0200

    remove useless imports in appRoutingModule

--- apps/user-app/src/app/app-routing.module.ts
+++ apps/user-app/src/app/app-routing.module.ts
@@ -5,15 +5,11 @@ import {
   redirectUnauthorizedTo,
 } from '@angular/fire/compat/auth-guard';
 import { HomePageModule } from './pages/home.page-module';
-import { JoinEventPageModule } from './pages/join-event.page-module';
-import { MyEventCheckGuard } from './pages/my-event-check.guard';
-import { EventIdResolver } from 'libs/event/src/lib/adapters/primary/ui/event-id.resolver';
 import { LoginPageModule } from './pages/login.page-module';
 import { RegisterPageModule } from './pages/register.page-module';
 import { AccessDeniedPageModule } from './pages/access-denied.page-module';
 import { FlowPageModule } from './pages/flow.page-module';
 import { NotFoundPageModule } from './pages/not-found.page-module';
-
 
 const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['login']);
 
